14c81b7cfb7b689081bc2c96db8a88deb1f2bc9f
Rupsa <rupsa1671@gmail.com>
2020-11-05 20:05:48 +0530

    adding sum to first file

--- firstFile.sh
+++ firstFile.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 echo "This is the first file"
 echo "This is the first initialization"
+echo "Enter the numbers you want to calculate"
+read -p "Enter number1" x
+read -p "Enter number2" y
+((sum=x+y))
+echo $sum
